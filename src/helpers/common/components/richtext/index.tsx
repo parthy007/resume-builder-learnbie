adcb3778529837ca1df38472dce39fb325e1b7ec
parth adhikari <parthadhikari1812@gmail.com>
2024-02-15 23:34:17 +0530

    added spell-checker

--- src/helpers/common/components/richtext/index.tsx
+++ src/helpers/common/components/richtext/index.tsx
@@ -1,26 +1,38 @@
 import { useRef, useEffect, memo, useState } from 'react';
 import 'jodit/build/jodit.min.css';
-
 import { LinkPlugin } from './plugins/link';
-
 import styles from './jodit.module.css';
+import Typo from 'typo-js';
+
+const dict = new Typo('en_US', false as any, false as any, { dictionaryPath: '/assets' });
 
 interface IRichtext {
   label: string;
+
   onChange: (htmlOutput: string) => void;
+
   value: string;
+
   name: string;
 }
 
 export const RichtextEditor = memo(({ label, onChange, value }: IRichtext) => {
   const editorContainerRef = useRef<HTMLTextAreaElement | null>(null);
+
   const editorRef = useRef<any>(null);
   const [editorInstanceCreated, setEditorInstanceCreated] = useState(false);
+  const [currentWord, setCurrentWord] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [caretPosition, setCaretPosition] = useState<{ top: number; left: number }>({
+    top: 0,
+    left: 0,
+  });
 
   useEffect(() => {
     if (editorContainerRef.current) {
       const initEditor = async () => {
         const { Jodit } = await import('jodit');
+
         const editor = Jodit.make(editorContainerRef.current as HTMLTextAreaElement, {
           showCharsCounter: false,
           showWordsCounter: false,
@@ -52,9 +64,45 @@ export const RichtextEditor = memo(({ label, onChange, value }: IRichtext) => {
 
   useEffect(() => {
     if (editorRef.current && editorInstanceCreated) {
-      editorRef.current.events.on('change', onChange);
+      editorRef.current.events.on('change', () => {
+        const newText = editorRef.current.value || '';
+        const textWithoutTags = newText.replace(/<\/?[^>]+(>|$)/g, '');
+        const words = textWithoutTags.split(/\s+/);
+        const newWord = words[words.length - 1];
+        setCurrentWord(newWord);
+        if (newWord.length > 2) {
+          const suggestions = dict.suggest(newWord);
+          setSuggestions(suggestions);
+        } else {
+          setSuggestions([]);
+        }
+        const selection = window.getSelection();
+        if (selection && selection.rangeCount > 0) {
+          const range = selection.getRangeAt(0);
+          if (range.startContainer && range.startContainer.parentElement) {
+            const rect = range.getClientRects()[0];
+            const editorRect = editorRef.current.container.getBoundingClientRect();
+            const caretPosition = {
+              top: rect ? rect.top - editorRect.top : 0,
+              left: rect ? rect.left - editorRect.left : 0,
+            };
+            setCaretPosition(caretPosition);
+          }
+        }
+      });
     }
   }, [onChange, editorInstanceCreated]);
+
+  const handleSuggestionClick = (suggestion: string) => {
+    const newText = editorRef.current.value || '';
+    const textWithoutTags = newText.replace(/<\/?[^>]+(>|$)/g, '');
+    const words = textWithoutTags.split(/\s+/);
+    words[words.length - 1] = suggestion;
+    const updatedText = words.join(' ');
+    editorRef.current.value = updatedText;
+    setCurrentWord(suggestion);
+    setSuggestions([]);
+  };
 
   return (
     <div className={`${styles.editor_wrapper} mb-4`}>
@@ -66,10 +114,35 @@ export const RichtextEditor = memo(({ label, onChange, value }: IRichtext) => {
       >
         <span>{label}</span>
       </div>
-      <textarea
-        ref={editorContainerRef}
-        className={`min-h-[200px] min-w-full bg-[rgba(0,0,0,0.06)]`}
-      ></textarea>
+
+      <div style={{ position: 'relative', display: 'block' }}>
+        <textarea
+          ref={editorContainerRef}
+          className={`min-h-[200px] min-w-full bg-[rgba(0,0,0,0.06)]`}
+        ></textarea>
+
+        {suggestions.length > 0 && (
+          <div
+            style={{
+              position: 'absolute',
+              top: caretPosition.top / 10 + 30,
+              left: caretPosition.left + 5,
+              backgroundColor: 'rgb(236 207 225)',
+              padding: '5px',
+              borderRadius: '10px',
+              zIndex: '99',
+            }}
+          >
+            <ul>
+              {suggestions.map((suggestion, index) => (
+                <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 });
